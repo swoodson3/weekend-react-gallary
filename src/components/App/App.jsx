f0045440af4aa2ec6a54a9fe1170dbf9a39d8e3e
Seth Woodson <sethwoodson3@gmail.com>
2023-04-08 13:02:58 -0500

    working-on-listitem

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList.jsx'
 
 
 function App() {
 
-  const [gallery, setGalleryList] = useState([]); 
+  const [gallery, setGalleryList] = useState([]);
+  
+  const fetchGalleryList = () => {
+    axios.get('/gallery').then((response) => {
+        setGalleryList(response.data);
+    }).catch((error) => {
+        console.log(`Error in Get ${error}`);
+        alert('Something went wrong!')
+    })
+}
+
+// ! Don't do this, it will potentially mess up Reacts timing 
+//fetchCreatureList();
+useEffect(() => {
+    // At this point, React is ready!
+    fetchGalleryList();
+}, []); // ! Remember the empty Array
 
   return (
+
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Gallery of My Life</h1>
@@ -20,7 +38,9 @@ function App() {
       <img src="images/Bath.png" width="100" height="100" />
       <img src="images/Christmas.png" width="100" height="100" />
       <img src="images/Playtime.png" width="100" height="100" />
-      <GalleryList />
+      <GalleryList 
+      gallery= {gallery}
+      />
     </div>
   );
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,37 +1,25 @@
 import { useState, useEffect } from 'react';
-// We must import axios in each component we want to use it 
 import axios from 'axios';
-import GalleryItem from '../GalleryItem.jsx';
+import GalleryItem from '..GalleryItem/GalleryItem.jsx';
 
-function GalleryList(gallery, setGalleryList) {
 
-    const fetchGalleryList = () => {
-        axios.get('/gallery').then((response) => {
-            setGalleryList(response.data);
-        }).catch((error) => {
-            console.log(`Error in Get ${error}`);
-            alert('Something went wrong!')
-        })
-    }
+function GalleryList({ gallery }) {
 
-    // ! Don't do this, it will potentially mess up Reacts timing 
-    //fetchCreatureList();
-    useEffect(() => {
-        // At this point, React is ready!
-        fetchGalleryList();
-    }, []); // ! Remember the empty Array
+
 
     return (
-        <>
-        {
-                    // ! .map happens in our List Component
-                    gallery.map((photo) => (
-                        <GalleryItem
-                        key={photo.id}
-                        />
-                    ))
-                }
-        </>
+
+        <div>
+            <h1>Gallery List</h1>
+            <p>
+                {gallery.map(picture => (
+                    <GalleryItem
+                        key={picture.id}
+                        picture={picture}
+                    />
+                ))}
+            </p>
+        </div>
     )
 
 }
