18792fe02df34a161b755f209e82d7859afee445
Seth Woodson <sethwoodson3@gmail.com>
2023-04-08 10:22:28 -0500

    images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" width="100" height="100" />
+      <img src="images/Birthday-Girl.png" width="100" height="100" />
+      <img src="images/Halloween.png" width="100" height="100" />
+      <img src="images/Bath.png" width="100" height="100" />
+      <img src="images/Christmas.png" width="100" height="100" />
+      <img src="images/Playtime.png" width="100" height="100" />
+    </div>
+  );
 }
 
 export default App;
